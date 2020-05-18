1521ba5a8306181e7fe5c2382f23524234eb401f
nlg734 <npsm111@gmail.com>
2020-05-17 23:57:27 -0500

    first commit, stage 1 complete

---
+++ Zookeeper/task/zookeeper/zookeeper.py
@@ -0,0 +1,9 @@
+# zookeeper.py
+# Natasha Graham
+# JetBrains
+
+print("I do love animals!")
+print("Start looking after animals...")
+print("Deer looks fine.")
+print("Bat looks happy.")
+print("Lion looks healthy.")
